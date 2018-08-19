38d0fd1edbfd9f2cddfe78dfdab14f7a300a7737
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2018-08-19 23:04:44 +0200

    Added first (smoke) Jest test

---
+++ MyAuthorizationApp/__tests__/AppTest.js
@@ -0,0 +1,10 @@
+// __tests__/Intro-test.js
+import React from 'react';
+import App from '../App';
+
+import renderer from 'react-test-renderer';
+
+test('renders correctly', () => {
+  const tree = renderer.create(<App />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
